09b8b4734227b2b9ba92b65fa7b58696833a1347
NIMCOO <nimco77001@gmail.com>
2023-12-05 09:23:27 +0000

    Finished Exercise

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Good Luck 💪🏾
+function findSeven(found){
+for(const onlySeven of found)
+console.log(onlySeven)
+}
+
+findSeven
+([1, 2, 3, 4, 5, 6, 7]) ="Found!"
+// 7 contains the number seven.
+
+findSeven([8, 6, 33, 100]) ="there is no 7 in the array"
+// None of the items contain 7 within them.
+
+findSeven([2, 55, 60, 97, 86]) = "there is no 7 in the array"
+//  None of the items contain 7 within them.
+
